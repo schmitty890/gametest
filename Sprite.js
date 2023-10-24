adc0b61b74c2c816a3faf5a88ccdf15f7b01ce0b
Jason Schmitt <schmitty890@gmail.com>
2023-10-24 16:11:25 -0400

    pretty code up

--- Sprite.js
+++ Sprite.js
@@ -1,12 +1,11 @@
 class Sprite {
   constructor(config) {
-
     //Set up the image
     this.image = new Image();
     this.image.src = config.src;
     this.image.onload = () => {
       this.isLoaded = true;
-    }
+    };
 
     //Shadow
     this.shadow = new Image();
@@ -16,14 +15,12 @@ class Sprite {
     }
     this.shadow.onload = () => {
       this.isShadowLoaded = true;
-    }
+    };
 
     //Configure Animation & Initial State
     this.animations = config.animations || {
-      idleDown: [
-        [0,0]
-      ]
-    }
+      idleDown: [[0, 0]],
+    };
     this.currentAnimation = config.currentAnimation || "idleDown";
     this.currentAnimationFrame = 0;
 
@@ -37,11 +34,6 @@ class Sprite {
 
     this.isShadowLoaded && ctx.drawImage(this.shadow, x, y);
 
-    this.isLoaded && ctx.drawImage(this.image,
-      0,0,
-      32,32,
-      x,y,
-      32,32
-    )
+    this.isLoaded && ctx.drawImage(this.image, 0, 0, 32, 32, x, y, 32, 32);
   }
 }
--- init.js
+++ init.js
@@ -1,8 +1,6 @@
 (function () {
-
   const overworld = new Overworld({
-    element: document.querySelector(".game-container")
+    element: document.querySelector(".game-container"),
   });
   overworld.init();
-
 })();
