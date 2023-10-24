74324dcb9d930772a61d934054124c891ba8447f
Jason Schmitt <schmitty890@gmail.com>
2023-10-24 16:07:44 -0400

    refactor and create game objects

--- Overworld.js
+++ Overworld.js
@@ -1,50 +1,34 @@
 class Overworld {
-  constructor(config) {
-    this.element = config.element;
-    this.canvas = this.element.querySelector(".game-canvas");
-    this.ctx = this.canvas.getContext("2d");
-  }
+ constructor(config) {
+   this.element = config.element;
+   this.canvas = this.element.querySelector(".game-canvas");
+   this.ctx = this.canvas.getContext("2d");
+ }
 
-  init() {
-    const image = new Image();
-    image.onload = () => {
-      this.ctx.drawImage(image, 0, 0);
-    };
-    image.src = "/images/maps/DemoLower.png";
+ init() {
+   const image = new Image();
+   image.onload = () => {
+     this.ctx.drawImage(image,0,0)
+   };
+   image.src = "/images/maps/DemoLower.png";
 
-    const x = 5;
-    const y = 6;
+   //Place some Game Objects!
+   const hero = new GameObject({
+     x: 5,
+     y: 6,
+   })
+   const npc1 = new GameObject({
+    x: 7,
+    y: 9,
+    src: "/images/characters/people/npc1.png"
+  })
 
-    const shadow = new Image();
-    shadow.onload = () => {
-      this.ctx.drawImage(
-        shadow,
-        0, //left cut
-        0, //top cut,
-        32, //width of cut
-        32, //height of cut
-        x * 16 - 8,
-        y * 16 - 18,
-        32,
-        32
-      );
-    };
-    shadow.src = "/images/characters/shadow.png";
+  setTimeout(() => {
+    hero.sprite.draw(this.ctx);
+    npc1.sprite.draw(this.ctx);
+  }, 200)
 
-    const hero = new Image();
-    hero.onload = () => {
-      this.ctx.drawImage(
-        hero,
-        0, //left cut
-        0, //top cut,
-        32, //width of cut
-        32, //height of cut
-        x * 16 - 8,
-        y * 16 - 18,
-        32,
-        32
-      );
-    };
-    hero.src = "/images/characters/people/hero.png";
-  }
+
+ }
+
 }
